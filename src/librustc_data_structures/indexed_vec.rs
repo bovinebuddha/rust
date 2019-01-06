37110b794d1a5806b57ab4ec3bfc01a5106c3b0f
Mathias Blikstad <mathias@blikstad.se>
2019-01-06 17:09:48 +0100

    Fix typo

--- src/librustc_data_structures/indexed_vec.rs
+++ src/librustc_data_structures/indexed_vec.rs
@@ -150,13 +150,13 @@ macro_rules! newtype_index {
                 self.as_usize()
             }
 
-            /// Extract value of this index as a usize.
+            /// Extract value of this index as a u32.
             #[inline]
             $v fn as_u32(self) -> u32 {
                 self.private
             }
 
-            /// Extract value of this index as a u32.
+            /// Extract value of this index as a usize.
             #[inline]
             $v fn as_usize(self) -> usize {
                 self.as_u32() as usize
